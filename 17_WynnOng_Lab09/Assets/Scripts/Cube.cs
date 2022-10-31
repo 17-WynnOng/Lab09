43556746d8ce673e331190831f9d6d7153876bf5
17-WynnOng <Wynnong12@gmail.com>
2022-10-31 12:00:56 +0800

    Buid inside

---
+++ 17_WynnOng_Lab09/Assets/Scripts/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public int health;
+    // Update is called once per frame
+    void Update()
+    {
+        DisableOnDeath();
+    }
+
+    public void DisableOnDeath()
+    {
+        if (health < 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
